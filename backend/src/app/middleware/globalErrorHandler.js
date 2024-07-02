ef9de94ac1af39ba57a5ff30e6344d4024b68efe
tanviAlam <tanvir9832@gmail.com>
2024-07-02 20:03:43 +0600

    feat: errorHandling middleware

---
+++ backend/src/app/middleware/globalErrorHandler.js
@@ -0,0 +1,7 @@
+const errorMiddleware=(err,req,res,next)=>{
+    err.message = err.message || "Internal server error" ;
+    err.statusCode = err.statusCode || 500;
+    res.status(err.statusCode).json({ message: err.message });
+}
+
+module.exports = errorMiddleware
