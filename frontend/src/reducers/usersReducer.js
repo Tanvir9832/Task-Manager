c24246f8970d0247368458e183eba686baf0c657
tanviAlam <tanvir9832@gmail.com>
2024-07-03 17:49:36 +0600

    feat: user reducer

--- frontend/src/reducers/usersReducer.js
+++ frontend/src/reducers/usersReducer.js
@@ -1,4 +1,4 @@
-import constants from "../constants/tasksConstants";
+import constants from "../constants/usersConstants";
 
 const initialState = {
   isLoading: false,
@@ -8,12 +8,31 @@ const initialState = {
 
 export const usersReducer = (state = initialState, action) => {
   switch (action.type) {
+    // register
     case constants.REGISTER_USER_REQUEST:
       return { ...state, isLoading: true };
     case constants.REGISTER_USER_SUCCESS:
-      return { ...state, user: action.payload, error: null };
+      return { ...state, user: [], error: null };
     case constants.REGISTER_USER_FAILED:
       return { ...state, user: [], error: action.payload };
+
+    // login
+    case constants.LOGIN_USER_REQUEST:
+      return { ...state, isLoading: true };
+    case constants.LOGIN_USER_SUCCESS:
+      return { ...state, user: action.payload, error: null };
+    case constants.LOGIN_USER_FAILED:
+      return { ...state, user: [], error: action.payload };
+
+    // check user
+    case constants.CHECK_USER_REQUEST:
+      return { ...state, isLoading: true };
+    case constants.CHECK_USER_SUCCESS:
+      return { ...state, user: action.payload, error: null };
+    case constants.CHECK_USER_FAILED:
+      return { ...state, user: [], error: action.payload };
+
+
     default:
       return state;
   }
